b0fe183f0f81adf7c9c51d8cfa5ca57d5c3b9af7
dungeonlol <dungeonnoob2008@gmail.com>
2021-07-05 19:59:01 -0400

    Added movement ability to rocketship.
    Added a boundary to the screen to prevent rocket going outside of screen.
    Added comments to each funcion with breif summary.

--- Player.py
+++ Player.py
@@ -6,7 +6,39 @@ class Player(GameGenerics):
     #Initialize the class member variables
     def __init__(self):
         super(Player, self).__init__()
+        self.playerX_change = 0
 
+    # Player function will draw the image across screen.
+    # param1 (screen): takes reference to player screen.
+    # param2 (image): takes a player sprite image.
+    # param3 (x_axis): takes the x axis position which is used to draw image.
+    # param4 (y_axis): takes the y axis position which is used to draw image.
+    # returns nothing
     def player(self, screen, image, x_axis=0, y_axis=0):
         player_img = pygame.image.load(image)
         screen.blit(player_img, (x_axis, y_axis))
+
+    # player_controls function allows users to move the sprite in the x axis.
+    # param1 (event): takes a player's inputs.
+    def player_controls(self, event):
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                self.playerX_change = -0.5
+            if event.key == pygame.K_RIGHT:
+                self.playerX_change = 0.5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                self.playerX_change = 0
+
+        return self.playerX_change
+
+    # boundary_control makes sure the rocketship sprite cannot move beyond the borders of the screen.
+    def boundary_control(self, x_axis):
+        if x_axis <= 0:
+            x_axis = 0
+        elif x_axis >= 736:
+            x_axis = 736
+
+        return x_axis
--- SpaceInvader.py
+++ SpaceInvader.py
@@ -13,20 +13,26 @@ class SpaceInvader(GameGenerics):
         self.screen = self.get_screen(800, 600)
         self.x_axis = 370
         self.y_axis = 480
-        self.set_display_caption_and_icon("Space Invader", "images/ufo.png")
+        self.set_display_caption_and_icon("Space Invaders", "images/ufo.png")
         self.main_game_loop()
 
     def main_game_loop(self):
         active = True
         my_player = Player()
         player_image = "Images/rocket.png"
+        changes = 0
 
         while active:
            self.screen.fill((0, 0, 0))
            for event in pygame.event.get():
                active = self.close_screen(event, active)
+               changes = my_player.player_controls(event)
 
+           self.x_axis += changes
+           self.x_axis = my_player.boundary_control(self.x_axis)
            my_player.player(self.screen, player_image, self.x_axis, self.y_axis)
            pygame.display.update()
+
+
 
 
