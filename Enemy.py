fd5881c67da074a3638b58b58913aa27734c8a9c
dungeonlol <dungeonnoob2008@gmail.com>
2021-07-11 20:11:09 -0400

    Added Enemy Movement and a background
    Added a enemy class
    Added enemy movement
    Added comments for new functions
    Added background for game

---
+++ Enemy.py
@@ -0,0 +1,61 @@
+from GameGenerics import *
+
+
+class Enemy(GameGenerics):
+
+    ##Constructor for Enemy class.
+    #Initializes the class member variables.
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.enemy_image = "images/alien.png"
+        self.enemy_img = pygame.image.load(self.enemy_image)
+        self.flag_move_right = True
+        self.flag_move_down = False
+
+    ## enemy function will draw the image across screen.
+    # param1 (screen): takes reference to player screen.
+    # param2 (x_axis): takes the x axis position which is used to draw image.
+    # param3 (y_axis): takes the y axis position which is used to draw image.
+    # returns nothing
+    def enemy(self, screen, x_axis=0, y_axis=0):
+        screen.blit(self.enemy_img, (x_axis, y_axis))
+
+    ## enemy_movements function moves enemies across the screen in the x axis.
+    # param1 (x_axis: takes position of x_axis.
+    # return integer x_axis
+    def enemy_movements(self, x_axis):
+
+        if x_axis < 0:
+            self.flag_move_right = True
+        elif x_axis > 736:
+            self.flag_move_right = False
+            self.flag_move_down = True
+        elif x_axis == 0:
+            self.flag_move_right = False
+            self.flag_move_down = True
+        elif x_axis < 736:
+            self.flag_move_down = False
+
+        if self.flag_move_right:
+            x_axis = 2
+        else:
+            x_axis = -2
+
+        return x_axis
+
+    ## enemy_move_down_one_row function moves enemies one row down after reaching the right edge of our screen.
+    # param1 (y_axis): takes position of y_axis.
+    # returns integer down_by.
+    def enemy_move_down_one_row(self, y_axis):
+
+        down_by = 0
+
+        if self.flag_move_down and y_axis <= 510:
+            down_by = 40
+        else:
+            down_by = 0
+
+        return down_by
+
+
+
--- SpaceInvader.py
+++ SpaceInvader.py
@@ -1,6 +1,8 @@
+import random
+
 import pygame.display
 from Player import Player
-
+from Enemy import Enemy
 from GameGenerics import *
 
 
@@ -13,25 +15,38 @@ class SpaceInvader(GameGenerics):
         self.screen = self.get_screen(800, 600)
         self.x_axis = 370
         self.y_axis = 480
+        self.x_axis_enemy = random.randint(0, 736)
+        self.y_axis_enemy = random.randint(0, 150)
         self.set_display_caption_and_icon("Space Invaders", "images/ufo.png")
+        self.background = pygame.image.load("images/background.png")
         self.main_game_loop()
+
 
     ## main_game_loop function sets the image of the rocket sprite, makes it so that everytime the sprite draws it erases the previous drawing, and initializes everything.
     def main_game_loop(self):
         active = True
         my_player = Player()
+        my_enemy = Enemy()
         player_image = "Images/rocket.png"
         changes = 0
 
         while active:
            self.screen.fill((0, 0, 0))
+           self.screen.blit(self.background, (0, 0))
            for event in pygame.event.get():
                active = self.close_screen(event, active)
                changes = my_player.player_controls(event)
 
            self.x_axis += changes
            self.x_axis = my_player.boundary_control(self.x_axis)
+
+           enemy_x_changes = my_enemy.enemy_movements(self.x_axis_enemy)
+           self.x_axis_enemy += enemy_x_changes
+           self.y_axis_enemy += my_enemy.enemy_move_down_one_row(self.y_axis_enemy)
+
+
            my_player.player(self.screen, player_image, self.x_axis, self.y_axis)
+           my_enemy.enemy(self.screen, self.x_axis_enemy, self.y_axis_enemy)
            pygame.display.update()
 
 
