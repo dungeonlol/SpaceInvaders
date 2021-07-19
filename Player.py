d856399453228ea560f1d2c6733c9d8273f6161a
dungeonlol <dungeonnoob2008@gmail.com>
2021-07-18 20:20:29 -0400

    messege

--- Player.py
+++ Player.py
@@ -7,6 +7,11 @@ class Player(GameGenerics):
     def __init__(self):
         super(Player, self).__init__()
         self.playerX_change = 0
+        self.player_image_path = "images/rocket.png"
+        self.player_img = pygame.image.load(self.player_image_path)
+        self.bullet_path = 'images/bullet.png'
+        self.bullet_img = pygame.image.load(self.bullet_path)
+        self.control_data = [0, "ready"]
 
     ## Player function will draw the image across screen.
     # param1 (screen): takes reference to player screen.
@@ -14,28 +19,38 @@ class Player(GameGenerics):
     # param3 (x_axis): takes the x axis position which is used to draw image.
     # param4 (y_axis): takes the y axis position which is used to draw image.
     # returns nothing
-    def player(self, screen, image, x_axis=0, y_axis=0):
-        player_img = pygame.image.load(image)
-        screen.blit(player_img, (x_axis, y_axis))
+    def player(self, screen,  x_axis=0, y_axis=0):
+        screen.blit(self.player_img, (x_axis, y_axis))
 
     ## player_controls function allows users to move the sprite in the x axis.
     # param1 (event): takes a player's inputs.
-    def player_controls(self, event):
+    # param2 (fire_state): takes whether the player is shooting or not.
+    # returns integer playerX_change
+    def player_controls(self, event, fire_state):
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                self.playerX_change = -0.5
+                self.playerX_change = -5
             if event.key == pygame.K_RIGHT:
-                self.playerX_change = 0.5
+                self.playerX_change = 5
+            if fire_state == "ready":
+                if event.key == pygame.K_SPACE:
+                    fire_state = "fire"
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 self.playerX_change = 0
 
-        return self.playerX_change
+
+
+        self.control_data[0] = self.playerX_change
+        self.control_data[1] = fire_state
+
+        return self.control_data
 
     ## boundary_control makes sure the rocketship sprite cannot move beyond the borders of the screen.
     # param1 (x_axis) takes the x axis position which is used to draw image.
+    # returns integer x_axis
     def boundary_control(self, x_axis):
         if x_axis <= 0:
             x_axis = 0
@@ -43,3 +58,11 @@ class Player(GameGenerics):
             x_axis = 736
 
         return x_axis
+
+    ## fire_bullets function will draw the image across screen.
+    # param1 (screen): takes reference to player screen.
+    # param2 (x_axis): takes the x axis position which is used to draw image.
+    # param3 (y_axis): takes the y axis position which is used to draw image.
+    # returns nothing
+    def fire_bullets(self, screen, x_axis=0, y_axis=0):
+        screen.blit(self.bullet_img, (x_axis + 16, y_axis + 10))
--- SpaceInvader.py
+++ SpaceInvader.py
@@ -17,37 +17,59 @@ class SpaceInvader(GameGenerics):
         self.y_axis = 480
         self.x_axis_enemy = random.randint(0, 736)
         self.y_axis_enemy = random.randint(0, 150)
+        self.x_axis_bullet = 0
+        self.y_axis_bullet = 480
         self.set_display_caption_and_icon("Space Invaders", "images/ufo.png")
         self.background = pygame.image.load("images/background.png")
         self.main_game_loop()
 
 
-    ## main_game_loop function sets the image of the rocket sprite, makes it so that everytime the sprite draws it erases the previous drawing, and initializes everything.
+    ## main_game_loop function handles all the operations of the game.
     def main_game_loop(self):
         active = True
         my_player = Player()
         my_enemy = Enemy()
-        player_image = "Images/rocket.png"
         changes = 0
+        fire_state = "ready"
 
         while active:
            self.screen.fill((0, 0, 0))
            self.screen.blit(self.background, (0, 0))
+
            for event in pygame.event.get():
                active = self.close_screen(event, active)
-               changes = my_player.player_controls(event)
+               control_data = my_player.player_controls(event, fire_state)
+               changes = control_data[0]
 
+               if fire_state == "ready":
+                   fire_state = control_data[1]
+                   self.x_axis_bullet = self.x_axis
+
+           # Draws the player across the x axis.
            self.x_axis += changes
            self.x_axis = my_player.boundary_control(self.x_axis)
+           my_player.player(self.screen, self.x_axis, self.y_axis)
 
+            # Draws the enemy across the x and y axis.
            enemy_x_changes = my_enemy.enemy_movements(self.x_axis_enemy)
            self.x_axis_enemy += enemy_x_changes
            self.y_axis_enemy += my_enemy.enemy_move_down_one_row(self.y_axis_enemy)
-
-
-           my_player.player(self.screen, player_image, self.x_axis, self.y_axis)
            my_enemy.enemy(self.screen, self.x_axis_enemy, self.y_axis_enemy)
            pygame.display.update()
+
+           if fire_state == "fire":
+
+               self.y_axis_bullet -= 4
+               my_player.fire_bullets(self.screen, self.x_axis_bullet, self.y_axis_bullet)
+
+               # If bullet goes past the screen this will reset bullet to starting position and make it stop moving.
+               if self.y_axis_bullet <= -5:
+                   fire_state = "ready"
+                   self.y_axis_bullet = 480
+
+           pygame.display.update()
+
+
 
 
 
